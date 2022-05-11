41c663dd49f09ccd88a2d8e09059e374c0dc172c
NikitaShaberov <99290177+NikitaShaberov@users.noreply.github.com>
2022-05-11 15:01:08 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -6,63 +6,61 @@ template<typename T>
 class TPQueue {
  private:
     struct ITEM {
-        T num;
+        T value;
         ITEM* next;
-        ITEM* prev;
+        ITEM* last;
     };
   ITEM* head;
   ITEM* tail;
 
-    ITEM* create(T num) {
+    ITEM* create(T value) {
     ITEM* item = new ITEM;
-    item->num = num;
+    item->value = value;
     item->next = nullptr;
-    item->prev = nullptr;
+    item->last = nullptr;
     return item;
 }
 
-
  public:
 
-    void push(const T& value) {
-        ITEM* num = head;
-        ITEM* item = create(value);
-        while (num && num->value.prior >= value.prior)
-            num = num->next;
-        if (!num && head) {
-            tail->next = item;
-            tail->next->prev = tail;
-            tail = item;
-        } else if (!num && !head) {
-            head = tail = item;
-        } else if (!num->prev) {
-            num->prev = item;
-            item->next = num;
-            head = item;
-        } else {
-            num->prev->next = item;
-            item->prev = num->prev;
-            item->next = num;
-            num->prev = item;
-        }
+ T pop() {
+    ITEM* num = head->next;
+    if (num) {
+      num->last = nullptr;
     }
+    T value = head->value;
+    delete head;
+    head = num;
+    return value;
+  }
 
-        T pop() {
-        if (head) {
-            ITEM* num = head->next;
-            if (num)
-                num->prev = nullptr;
-            T value = head->value;
-            delete head;
-            head = num;
-            return value;
-        } else {
-            throw std::string("Empty!");
-        }
-    }
+
+    TPQueue() :head(nullptr), tail(nullptr) {}
+
+    void push(T value) {
+      ITEM* num = head;
+      ITEM* item = create(value);
+      while (num && num->value.prior >= value.prior)
+        num = num->next;
+      if (!num && head) {
+        tail->next = item;
+        tail->next->last = tail;
+        tail = item;
+      } else if (!num && !head) {
+        head = tail = item;
+      } else if (!num->last) {
+        num->last = item;
+        item->next = num;
+        head = item;
+      } else {
+        num->last->next = item;
+        item->last = num->last;
+        item->next = num;
+        num->last = item;
+      }
+  }
+  
 };
-
-
 
 struct SYM {
   char ch;
